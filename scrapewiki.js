d1737c047e97a8ed1ff65408d8bea6ea0a43780a
Kushan Joshi <0o3ko0@gmail.com>
2017-03-15 13:53:55 +0530

    add top places

--- scrapewiki.js
+++ scrapewiki.js
@@ -2,6 +2,9 @@ var cheerio = require('cheerio');
 var fetch = require('node-fetch');
 var fs = require('fs');
 
+const googleURL = (city) => `https://www.google.co.in/search?q=most+popular+places+in+${city}&oq=most+popular+places+in+${city}&ie=UTF-8`;
+
+
 function scraper(url, type) {
     return fetch(url)
     .then(r => r.text())
@@ -21,14 +24,44 @@ function scraper(url, type) {
             });
         }
         titles = titles.filter(x => x)
+        .filter(x => x.toLowerCase().indexOf('wikipedia') === -1)
+        .filter(x => x.split(' ').length < 6);
         return titles;
     })
     .catch(console.error);
 }
 
+function googleScrapper(url, className) {
+    return fetch(url)
+        .then(r => r.text())
+        .then(res => {
+            var titles = []
+            $ = cheerio.load(res);
+            $('#search').find('ol').children('div').find('span').each(function(x) {
+                if ($(this).text().charAt(0) === ',') {
+                    titles.push($(this).text().slice(1));
+                }
+            });
+            return titles.filter(x=> x);
+        });
+}
 
-scraper(process.env.URL, process.env.TAG).then(r => {
-    var fileName = process.env.URL.split('/');
-    fileName = fileName[fileName.length - 1] + '.txt';
-    fs.writeFileSync(fileName, r.join('\n'), 'utf8');
-});
+if (process.env.GOOGLE) {
+    const url = googleURL(process.env.GOOGLE);
+    const className = process.env.CLASS;
+    googleScrapper(url, className).then(r => {
+        var fileName = 'google-' + process.env.GOOGLE + '.txt';
+        writeToFile(fileName, r);
+    });
+} else {
+    scraper(process.env.URL, process.env.TAG).then(r => {
+        var fileName = process.env.URL.split('/');
+        fileName = fileName[fileName.length - 1] + '.txt';
+        writeToFile(fileName, r);
+    });
+}
+
+
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data.join('\n'), 'utf8');
+}
