205a83343fcc19918cf4f8f8a3a0a911f754bc6c
Kushan Joshi <0o3ko0@gmail.com>
2017-03-15 13:05:10 +0530

    node scrape

---
+++ scrapewiki.js
@@ -0,0 +1,34 @@
+var cheerio = require('cheerio');
+var fetch = require('node-fetch');
+var fs = require('fs');
+
+function scraper(url, type) {
+    return fetch(url)
+    .then(r => r.text())
+    .then(res => {
+        var titles = []
+        $ = cheerio.load(res);
+        if (type == 'ul') {
+            $(type).children().each(function (x) {
+                titles.push($(this).children('a').attr('title'));
+            });
+           
+        }
+        else if (type === 'table') {
+            console.log('here')
+            $('table').children('tr').each(function (x) {
+               titles.push($(this).children('td').find('a').attr('title'));
+            });
+        }
+        titles = titles.filter(x => x)
+        return titles;
+    })
+    .catch(console.error);
+}
+
+
+scraper(process.env.URL, process.env.TAG).then(r => {
+    var fileName = process.env.URL.split('/');
+    fileName = fileName[fileName.length - 1]
+    fs.writeFileSync(fileName, r.join('\n'), 'utf8');
+});
